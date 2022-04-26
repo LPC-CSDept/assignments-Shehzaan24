cef214e269e4e44477bdd5f5aebf6f42dac4fd05
Shehzaan24 <shehzaansinghus@gmail.com>
2022-04-26 14:44:20 -0700

    finished_reading_code

--- quiz5/quiz5.4.2.cpp
+++ quiz5/quiz5.4.2.cpp
@@ -6,6 +6,30 @@ int main()
 {
     ifstream inputFile;
     
+  // Variables
+    int empID, num_emp, number, count;
+    string emp_name, dep_name;
+    double salary, total, average;
+
+
+
+    cout << "We are going to read all the information from the employee file!"<< endl;
+
+    inputFile.open("employee.txt");
+
+    // Uing a loop to open the file multiple times and read it 
+
+    while (inputFile >> count)
+    {
+        cout << empID << emp_name << dep_name << salary<< endl;
+    }
+
+    // closing the file
+    inputFile.close();
+
+
+    return 0;
+
 
 
 
